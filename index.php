49788d412be2c56b53dd07841a6c7ce7538d3968
Cihan <cihanozkaninc@gmail.com>
2018-12-21 23:29:25 +0100

    cv

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $namaz = new Namaz();
 	header('Access-Control-Allow-Origin: *');
 	header('Content-type: application/json');
 
-	$vakitler = $namaz->vakitler(13980);
+	$vakit = $namaz->vakitler(13980);
 
 	echo json_encode($vakitler, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
