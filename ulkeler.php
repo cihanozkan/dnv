c084a167bb0ed7c534e875450ddb29d430bdcecb
Cihan <cihanozkaninc@gmail.com>
2020-11-03 22:44:49 +0100

    Added ulkeler.php

--- ulkeler.php
+++ ulkeler.php
@@ -32,7 +32,7 @@ $namaz = new Namaz();
 // $namaz->ilceIsimleri = $ilceIsimleri;
 //
 // // ülkelerin çekilmesi
-// $ulkeler = $namaz->ulkeler();
+ $ulkeler = $namaz->ulkeler();
 
 // $sehirler = $namaz->sehirler(2);
 //
@@ -42,19 +42,19 @@ $namaz = new Namaz();
 
 // $_GET["cityNumber"] = $cityNumber;
 // $_GET["cityNumber"] = $cityNumbero;
-$cityNumbero = $_GET["cityNumber"];
-
-	header('Access-Control-Allow-Origin: *');
-	header('Content-type: application/json');
-	// 1 hele maand rotterdam
-	// $vakitler = $namaz->vakit(13980);
+//$cityNumbero = $_GET["cityNumber"];
+	
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+// 1 hele maand rotterdam
+// $vakitler = $namaz->vakit(13980);
 // 1 dag alleen rotterdam
-	// $vakitler = $namaz->vakitler(13980);
-	$vakitler = $namaz->vakitler($cityNumbero);
-
-	echo json_encode($vakitler, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-//	echo json_encode($ulkeler, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-
+// $vakitler = $namaz->vakitler(13980);
+//$vakitler = $namaz->vakitler($cityNumbero);
+	
+//	echo json_encode($vakitler, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+echo json_encode($ulkeler, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+	
 // print_r($ulkeler);
 // print_r($sehirler);
 // print_r($ilceler);
